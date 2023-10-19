52fa9fc4d2f9cef64f2e823109aa9b086bbb8516
Adham <Adhammorsy75@gmail.com>
2023-10-19 19:12:19 -0400

    V2.4

---
+++ controllers/usersController.js
@@ -0,0 +1,49 @@
+const { User } = require('../models');
+const viewUsers = async (req, res) => {
+    try {
+        const users = await User.find()
+        res.json(users)
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+}
+
+const deleteUser = async (req, res) => {
+    try {
+        const id = req.params.id
+        const userDelete = await User.findByIdAndDelete(id)
+        if (userDelete) {
+            return res.status(200).send('removed user')
+        }
+        throw new Error("User not in database")
+    } catch (e) {
+        return res.status(500).send(e.message)
+    }
+}
+const editUser = async (req, res) => {
+    try {
+        const id = req.params.id
+        const editUser = await User.findByIdAndUpdate(id, req.body, { new: true })
+        if (editUser) {
+            return res.status(200).json(editUser)
+        } 
+    } catch (e) {
+        return res.status(500).send(e.message)
+    }
+}
+const User = async (req, res) => {
+try { const user = await new User(req.body)
+        await user.save()
+        return res.status(201).json({
+            user
+        })
+    } catch (e) {
+        return res.status(500).send(e.message)
+    }
+}
+module.exports = {
+    deleteUser,
+    viewUsers,
+    User,
+    editUser
+}
--- server.js
+++ server.js
@@ -5,13 +5,41 @@ const cors = require('cors')
 const PORT = process.env.PORT || 3001;
 const bodyParser = require('body-parser')
 const db = require('./db');
+const uController = require("./controllers/usersController")
+const rController = require("./controllers/repliesController")
+const pController = require("./controllers/postsController")
 
-
-app.use(cors())
-app.use(logger('dev'))
-app.use(bodyParser.json())
 app.use(express.urlencoded( { extended: true}))
 
+app.use(cors())
+
+app.use(logger('dev'))
+
+app.use(bodyParser.json())
+
+app.get('/post', pController.viewPosts)
+
+app.get('/replies', rController.viewReplies)
+
+app.get('/user', uController.viewUsers)
+
+app.post('/post', pController.post)
+
+app.post('/replies', rController.reply)
+
+app.post('/user', uController.user)
+
+app.delete('/post/:id', pController.deletePost)
+
+app.delete('/replies/:id', rController.deleteReply)
+
+app.delete('/user/:id', uController.deleteUser)
+
+app.put('/post/:id', pController.editPost)
+
+app.put('/replies/:id', rController.editReply)
+
+app.put('/users/:id', uController.editUser)
 
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
