e3a1048216de1c32a9da96780a6a1194bd5a525d
Adham <Adhammorsy75@gmail.com>
2023-10-20 00:01:58 -0400

    V5

--- client/script.js
+++ client/script.js
@@ -1,39 +1,49 @@
-
-
 document.addEventListener("DOMContentLoaded", function () {
-  const display = document.getElementById('snap')
-  const hosturl = "http://localhost:3009/post"
-  async function viewPosts() {
-    try { 
-        const host = await axios.get(`${hosturl}`)
+    // Wait for the DOM to be fully loaded before executing the code.
+    
+    const display = document.getElementById('snap');
+    // Get the HTML element with the id 'snap' and store it in the 'display' variable.
+  
+    const hosturl = "http://localhost:3009/post";
+    // Define the base URL for the API you want to fetch data from.
+  
+    async function viewPosts() {
+      // Define an asynchronous function 'viewPosts' to fetch and display posts.
+  
+      try {
+        // Start a try-catch block to handle potential errors.
+        const host = await axios.get(`${hosturl}`);
+        // Send an HTTP GET request to the 'hosturl' and store the response in 'host'. 'axios' is presumably a library used for making HTTP requests.
+  
         if (host.data.length > 0) {
-            display.innerHTML = ""
-            host.data.forEach(x => {
-                display.innerHTML += 
-                `
-                <h3>${x.username}</h3>
-                <p>${x.post}</p>
-                
-                
-                `
-            });
+          // Check if there are posts in the response data.
+          display.innerHTML = "";
+          // Clear the inner HTML content of the 'display' element.
+  
+          host.data.forEach(x => {
+            // Loop through the data and create HTML elements for each post.
+            display.innerHTML += 
+            `
+            <h3>${x.username}</h3>
+            <p>${x.post}</p>
+            `
+          });
         } else {
-            const postsContent = document.getElementById('snap')
-            postsContent.innerHTML = "No posts found"
+          // If no posts are found, display a message.
+          display.innerHTML = "No posts found";
         }
-
-    } catch (e) {
-
-        const postsContent = document.getElementById('snap')
+      } catch (e) {
+        // Handle errors that may occur during the execution of the try block.
+        const postsContent = document.getElementById('snap');
         console.error("Error:", e);
+        // Log the error to the console.
+  
         postsContent.innerHTML = "Error"; e
+        // Display an error message in the 'snap' element.
+      }
     }
-}
-
-viewPosts()
-
-
-
-
-
-})
+  
+    viewPosts();
+    // Call the 'viewPosts' function to start fetching and displaying posts.
+  });
+  
--- db/index.js
+++ db/index.js
@@ -1,16 +1,18 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 mongoose
     .connect('mongodb://127.0.0.1:27017/SnapDatabase')
+    // Connect to the MongoDB database with the specified connection URL.
     .then(() => {
-        console.log('Successfully connected to MongoDB.')
+        console.log('Successfully connected to MongoDB.');
+        // If the connection is successful, log a success message.
     })
     .catch(e => {
-        console.error('Connection error', e.message)
-    })
+        console.error('Connection error', e.message);
+        // If there's an error during the connection attempt, log an error message with the details.
+    });
 
+const db = mongoose.connection;
 
-
-const db = mongoose.connection
-
-module.exports = db
+module.exports = db;
+// Export the database connection object for use in other parts of your application.
